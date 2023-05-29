334147e913e1ab4e8229bb9791f287b66ae8cfb9
Abhirup <abhirup@rhymetech.com>
2023-05-29 21:28:28 +0530

    Added follow flow

--- src/utils/dboperations.js
+++ src/utils/dboperations.js
@@ -1,4 +1,5 @@
 const { createClient } = require('@supabase/supabase-js');
+const axios = require('axios');
 
 const supabaseUrl = process.env.REACT_APP_SUPA_URL;
 const supabaseKey = process.env.REACT_APP_SUPA_KEY;
@@ -76,6 +77,89 @@ export async function followUser(wallet_address,followed_address)
         });
     //create callback or modify
     console.log("followed")
+    const currentUserCallback = await database
+    .from('user_details')
+    .select()
+    .eq("wallet_address",wallet_address);
+
+    if(currentUserCallback.data.callback_id === null)
+    {
+      //new callback to create
+      let data = JSON.stringify({
+        network: "devnet",
+        addresses: [
+          followed_address
+        ],
+        "callback_url": "https://4c38-2405-201-8010-50c5-c14d-5105-29a5-f9e0.ngrok-free.app/callback/test",
+        "events": [
+          "SOL_TRANSFER"
+        ]
+      });
+      const endpoint = process.env.SHYFT_API_KEY;
+      
+      await axios({
+        url: `${endpoint}callback/create`,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.SHYFT_API_KEY,
+        },
+        data : data
+      })
+        .then(async (res) => {
+          if (res.data.success === true) {
+            const createdCallbackId = res.data.result.id;
+            const { error } = await database
+              .from('user_details')
+              .update({ callback_id: createdCallbackId })
+              .eq('wallet_address', wallet_address);
+            console.log("callback id to db");
+            console.log(error);
+          }
+        })
+        .catch((err) => {
+          console.warn(err);
+        });
+
+    }
+    else
+    {
+      const callbackIdmModify = currentUserCallback.data.callback_id;
+      const currentUserfollowed = await database
+        .from('user_follow')
+        .select()
+        .eq("wallet_address",wallet_address);
+      var followAddresses = [];
+      currentUserfollowed.data.forEach((follow_addr) => {
+        followAddresses.push(follow_addr.followed_address)
+      })
+      let data = JSON.stringify({
+        id: callbackIdmModify,
+        addresses: [
+          ...followAddresses,followed_address
+        ]
+      });
+
+      const endpoint = process.env.SHYFT_API_KEY;
+      await axios({
+        url: `${endpoint}callback/update`,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.SHYFT_API_KEY,
+        },
+        data : data
+      })
+        .then(async (res) => {
+          if (res.data.success === true) {
+            console.log(res.data.result);
+          }
+        })
+        .catch((err) => {
+          console.warn(err);
+        });
+
+    }
   }
   else
   {
