31fa917401eaf9ba56ed8bd0f4e549e34adb9598
Abhirup <abhirup@rhymetech.com>
2023-07-20 16:02:39 +0530

    Added top bar fixes

--- src/FeedComponent.js
+++ src/FeedComponent.js
@@ -18,21 +18,24 @@ const FeedComponent = ({popup,setPopUp,currentWallet}) => {
     return ( 
         <div>
             <div className={styles.background_super}>
-                <div className="container pt-5 pb-1">
-                    <div className="d-flex justify-content-between">
-                        <div className={styles.main_heading}>
-                            Your Feed
-                        </div>
-                    <div className="ps-1">
-                        <div className={styles.select_container}>
-                            <select value={cluster} onChange={(e) => changeCluster(e.target.value)}>
-                                <option value="mainnet-beta">Mainnet</option>
-                                <option value="devnet">Devnet</option>
-                                <option value="testnet">Testnet</option>
-                            </select>
+                <div className="container-lg pt-5 pb-1">
+                    <div className={styles.your_feed_heading_container}>
+                        <div className="d-flex flex-wrap justify-content-between">
+                            <div className={styles.main_heading}>
+                                Your Feed
+                            </div>
+                            <div className="ps-1">
+                                <div className={styles.select_container_feed}>
+                                    <select value={cluster} onChange={(e) => changeCluster(e.target.value)}>
+                                        <option value="mainnet-beta">Mainnet</option>
+                                        <option value="devnet">Devnet</option>
+                                        <option value="testnet">Testnet</option>
+                                    </select>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    </div>
+                    
                     {/* <SearchComponent popup={popup} setPopUp={setPopUp} currentWallet={currentWallet} /> */}
                     
                     
