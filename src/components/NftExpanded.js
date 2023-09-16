86571efa41cc77d44424d76628d6dbad3bea79a3
Abhirup <abhirup@rhymetech.com>
2023-09-16 14:38:38 +0530

    Added top header, and external url color change

--- src/components/NftExpanded.js
+++ src/components/NftExpanded.js
@@ -268,7 +268,6 @@ const NftExpanded = ({ nft, cluster }) => {
               <p className={styles.section_desc}>{nft.description ?? "--"}</p>
             </motion.div>
             {(nft.external_url !== "") && <motion.div className={styles.nft_section} initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} viewport={{ once: true }} transition={{ delay: 0.5 }} style={{paddingBottom: "24px"}}>
-              <h6 className={styles.section_heading}>External URL</h6>
               <a className={styles.section_desc_link} href={nft?.external_url ?? ""} target="_blank" rel="noreferrer">{nft.external_url ?? "--"}</a>
             </motion.div>}
             <div className={styles.nft_image_container_mob}>
